796f976665a1eb8d1f9b8c2131c48a772f1968d3
muriithiderro <muriithiderrick56@gmail.com>
2018-05-20 18:01:26 +0300

    LOGIN FORM
    Add login form and data required by the user
    and the validators to confirm data input.

---
+++ app/auth/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
+from ..models import User
+from wtforms.fields.html5 import EmailField
+
+class RegistrationForm(FlaskForm):
+    email = EmailField('Your Email Address',validators=[Required(),Email(message="I don't like your email.")])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(),
+    EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
+
+
+    # custom validators
+    # def validate_email(self,data_field):
+    # 	if User.query.filter_by(email=data_field).first():
+    # 		raise ValidationError("email already exist!!")
+
+    # def validate_username(self, data_field):
+    # 	if User.query.filter_by(username=data_field).first():
+    # 		raise ValidationError("username already exists!!")
+
+class LoginForm(FlaskForm):
+	email = StringField('Your Email Address', validators=[Required(),Email()])
+	password = PasswordField('Password', validators=[Required()])
+	remember = BooleanField('Remember me')
+	submit = SubmitField('Sign In')
