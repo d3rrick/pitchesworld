3d6f1432d0dee057620dd78a100b790fdac3f9d7
muriithiderro <muriithiderrick56@gmail.com>
2018-05-20 18:08:30 +0300

    AUTH VIEW METHODS
    Add three methods to handle login,
    registration and logout functionalities
    of the user.

---
+++ app/auth/views.py
@@ -0,0 +1,97 @@
+from flask import render_template,redirect,url_for,flash
+from flask_login import login_user,login_required,logout_user
+from . import auth
+from ..models import User
+from .forms import RegistrationForm,LoginForm
+from ..import db
+from ..import bcrypt
+from .. import login_manager
+from sqlalchemy import exc
+
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(user_id)
+
+
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+	login_form = LoginForm()
+
+	if login_form.validate_on_submit():
+		user = User.query.filter_by(email=login_form.email.data).first()
+		if user and bcrypt.check_password_hash(user.password_hash, login_form.password.data):
+			login_user(user,login_form.remember.data)
+			# flash("succesifull")
+			return redirect(url_for('main.categories'))
+		else:
+			print("unsuccessful")
+				
+
+	return render_template('auth/login.html', login_form=login_form)
+
+@auth.route('/register', methods=['GET','POST'])
+def register():
+	form = RegistrationForm()
+
+	if form.validate_on_submit():
+		hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+		user = User(username=form.username.data,email=form.email.data,password_hash=hashed_password)
+		db.session.add(user)
+		db.session.commit()
+		flash("invalid arguments", "danger")
+		return redirect(url_for('auth.login'))
+	return render_template('auth/register.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	flash('You have been logged out.',"danger")
+	return redirect(url_for('main.index'))
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
