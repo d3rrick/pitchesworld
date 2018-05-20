1fb9b8fb8761f750f25f016ae9b1006c84fe39a5
muriithiderro <muriithiderrick56@gmail.com>
2018-05-20 18:16:25 +0300

    MAIN BLUEPRINT FORMS
    Add a form to handle user editing data eg bio.

---
+++ app/main/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,SubmitField,TextAreaField,RadioField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
+# from ..models import User
+
+class UpdateProfile(FlaskForm):
+	bio = TextAreaField("Tell us about you.", validators=[Required()])
+	submit = SubmitField('submit')
+
+
+class PitchForm(FlaskForm):
+	body = TextAreaField("whats your pitch ?",validators=[Required()])
+	category = RadioField('Label', choices=[('pickup lines','pickup lines'), ('interview pitch','interview pitch'),('product pitch','product pitch'),('promotion pitch','promotion pitch')])
+	submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+	body = TextAreaField('',validators=[Required()])
+	submit = SubmitField()
