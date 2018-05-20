e655ef50345b411ff7ba8c4e071582f29f78a60b
muriithiderro <muriithiderrick56@gmail.com>
2018-05-20 19:43:27 +0300

    OS ENVIRONMENT VARIABLES
    Change the configutaion variables from the config file to the os
    environ variables.

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 
 class Config:
-	SECRET_KEY = "mysososossecretkey"
+	SECRET_KEY = os.environ.get('SECRET_KEY')
+	# secret
 	SQLALCHEMY_TRACK_MODIFICATIONS=True
-	SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/pitch"
+	SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+		# "postgresql://postgres:postgres@localhost/pitch"
 	UPLOADED_PHOTOS_DEST = "app/static/photos"
 
 	# email configurations
